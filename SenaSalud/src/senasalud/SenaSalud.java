81601200c55dd74d72af21f05c424693ef564aad
HarveyGonzalez <sala@HOR-SALA501-006.unihorizonte.salas>
2019-10-29 16:06:20 -0500

    Mi primer Commit

---
+++ SenaSalud/src/senasalud/SenaSalud.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package senasalud;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sala
+ */
+public class SenaSalud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner opc = new Scanner(System.in);
+    }
+    
+}
